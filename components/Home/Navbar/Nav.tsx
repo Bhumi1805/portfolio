b64bee6d26bd20231fe5d5872601cd493d4c3b61
Bhumi1805 <bhumimulani18@gmail.com>
2025-02-19 12:00:29 +0530

    navbar

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { navLinks } from '@/constant/constant'
+import { HiBars3BottomRight } from 'react-icons/hi2'
 
-const Nav = () => {
+//define props type
+
+type Props={
+  openNav:()=>void;
+}
+
+const Nav = ({openNav}:Props) => {
+
+  const [navBg,setNavBg]=useState(false)
+    useEffect(()=>{
+      const handler=()=>{
+        if(window.scrollY>=90)
+         {setNavBg(true)}
+        if(window.scrollY<90)
+        {setNavBg(false)}
+
+      }
+      window.addEventListener("scroll",handler)
+      return()=>removeEventListener("scroll",handler)
+    }  
+  )
+
   return (
-    <div className="fixed h-[12vh] z-[10] bg-blue-950 w-full">
+    <div className={`fixed 
+      ${navBg ? 'bg-[#240b39]':"fixed"} h-[12vh] z-[10] w-full transition-all duration-200`}>
       <div className='flex items-center h-full justify-between w-[95%] sm:w=[90%] xl:[w-80%] mx-auto'>
         {/*Logo*/}
         <Image src="/images/BM_logo.png"
@@ -14,13 +38,18 @@ const Nav = () => {
         height={170}
         className="ml-[-1.5 rem] sm:ml-0"
         />
-        {/*Nav links*/}
+        {/*Nav links*/} 
         <div className='flex items-center space-x-10'>
+          <div className='hidden lg:flex items-center space-x-8'>
           {navLinks.map((navlink)=>{
             return <Link key={navlink.id} href={navlink.url}>
-              <p>{navlink.label}</p>
+              <p className="nav__link">{navlink.label}</p>
             </Link>
           })}
+          </div>
+          {/* Burger */}
+          <HiBars3BottomRight onClick={openNav} 
+          className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
         </div>
       </div>
     </div>
