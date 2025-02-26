7c9f86554a3ae586b2bc347e6d0b1a40ec191a5a
Bhumi1805 <bhumimulani18@gmail.com>
2025-02-26 13:44:24 +0530

    skills modified

--- components/Home/Skills/SkillsCard.tsx
+++ components/Home/Skills/SkillsCard.tsx
@@ -1,19 +1,16 @@
 'use client';
 import Image from 'next/image';
 import React from 'react'
-import Tilt from 'react-parallax-tilt'
-
 
 type Props={
     skill: {
         id: number;
         title: string;
         image: string;
-        percent: string;
     }
 }
 const SkillsCard = ({skill}:Props) => {
-  const{image,title,percent}=skill
+  const{image,title}=skill
   return (
     <div className='p-6 hover:bg-blue-900 duration-300 transition-all cursor-pointer text-center rounded-lg bg-gray-900'>
         <Image src={image} alt={title} width={80} height={80} className='object-cover mx-auto'></Image>
